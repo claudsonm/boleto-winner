22514b3846ec5058de50756d917bbf120ab22742
Claudson Martins <claudson@outlook.com>
2020-10-05 09:04:15 -0300

    Refatorado teste para usar singleton

--- tests/BillFactoryTest.php
+++ tests/BillFactoryTest.php
@@ -4,17 +4,42 @@ namespace Claudsonm\BoletoWinner\Tests;
 
 use Claudsonm\BoletoWinner\Boleto;
 use Claudsonm\BoletoWinner\Convenio;
-use Claudsonm\BoletoWinner\Tests\Dummies\DummyBill;
 use Claudsonm\BoletoWinner\Exceptions\BoletoWinnerException;
 use Claudsonm\BoletoWinner\Factories\BillFactory;
+use Claudsonm\BoletoWinner\Tests\Dummies\DummyBill;
 use PHPUnit\Framework\TestCase;
 
 class BillFactoryTest extends TestCase
 {
+    private BillFactory $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory = BillFactory::getInstance();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->factory->unload(DummyBill::class);
+    }
+
+    /** @test */
+    public function it_ensures_the_factory_class_is_a_singleton()
+    {
+        $firstFactory = BillFactory::getInstance();
+        $secondFactory = BillFactory::getInstance();
+
+        $this->assertSame($firstFactory, $secondFactory);
+    }
+
     /** @test */
     public function it_checks_that_boleto_and_convenio_are_loaded_by_default_in_the_available_bills()
     {
-        $bills = (new BillFactory())->getBills();
+        $bills = $this->factory->getBills();
 
         $this->assertTrue(in_array(Boleto::class, $bills));
         $this->assertTrue(in_array(Convenio::class, $bills));
@@ -29,15 +54,14 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage("The class `{$class}` must be a subclass of `Claudsonm\\BoletoWinner\\Bill`.");
 
-        (new BillFactory())->load($class);
+        $this->factory->load($class);
     }
 
     /** @test */
     public function it_can_load_subclasses_of_bill()
     {
-        $factory = new BillFactory();
-        $factory->load(DummyBill::class);
-        $bills = $factory->getBills();
+        $this->factory->load(DummyBill::class);
+        $bills = $this->factory->getBills();
 
         $this->assertCount(3, $bills);
         $this->assertTrue(in_array(DummyBill::class, $bills));
@@ -46,10 +70,9 @@ class BillFactoryTest extends TestCase
     /** @test */
     public function it_doesnt_load_the_same_bill_twice()
     {
-        $factory = new BillFactory();
-        $factory->load(DummyBill::class);
-        $factory->load(DummyBill::class);
-        $bills = $factory->getBills();
+        $this->factory->load(DummyBill::class);
+        $this->factory->load(DummyBill::class);
+        $bills = $this->factory->getBills();
 
         $this->assertCount(3, $bills);
         $this->assertTrue(in_array(DummyBill::class, $bills));
@@ -61,7 +84,7 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage('You must provide a numeric string.');
 
-        (new BillFactory())->make('');
+        $this->factory->make('');
     }
 
     /** @test */
@@ -70,7 +93,7 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage('You must provide a numeric string.');
 
-        (new BillFactory())->make('only text given');
+        $this->factory->make('only text given');
     }
 
     /** @test */
@@ -79,14 +102,13 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage('The value `123abc456d` given is not a valid barcode or writable line.');
 
-        (new BillFactory())->make('123abc456d');
+        $this->factory->make('123abc456d');
     }
 
     /** @test */
     public function its_creates_a_boleto_object_from_a_valid_numeric_writable_line()
     {
-        $factory = new BillFactory();
-        $bill = $factory->make('07790001161200000050003967606728182130000200000');
+        $bill = $this->factory->make('07790001161200000050003967606728182130000200000');
 
         $this->assertInstanceOf(Boleto::class, $bill);
         $this->assertSame('07791821300002000000001112000000500396760672', $bill->getBarcode());
@@ -95,8 +117,7 @@ class BillFactoryTest extends TestCase
     /** @test */
     public function its_creates_a_convenio_object_from_a_valid_numeric_writable_line()
     {
-        $factory = new BillFactory();
-        $bill = $factory->make('846200000012379800820894999931930112030367115992');
+        $bill = $this->factory->make('846200000012379800820894999931930112030367115992');
 
         $this->assertInstanceOf(Convenio::class, $bill);
         $this->assertSame('84620000001379800820899999319301103036711599', $bill->getBarcode());
@@ -105,8 +126,7 @@ class BillFactoryTest extends TestCase
     /** @test */
     public function it_creates_a_boleto_object_from_a_valid_formatted_writable_line()
     {
-        $factory = new BillFactory();
-        $bill = $factory->make('23793.38128 60023.848041 44000.063303 1 82130000300000');
+        $bill = $this->factory->make('23793.38128 60023.848041 44000.063303 1 82130000300000');
 
         $this->assertInstanceOf(Boleto::class, $bill);
         $this->assertSame('23791821300003000003381260023848044400006330', $bill->getBarcode());
@@ -115,8 +135,7 @@ class BillFactoryTest extends TestCase
     /** @test */
     public function it_creates_a_convenio_object_from_a_valid_formatted_writable_line()
     {
-        $factory = new BillFactory();
-        $bill = $factory->make('84690000001 5 24490082089 9 99993193010 4 99453529299 3');
+        $bill = $this->factory->make('84690000001 5 24490082089 9 99993193010 4 99453529299 3');
 
         $this->assertInstanceOf(Convenio::class, $bill);
         $this->assertSame('84690000001244900820899999319301099453529299', $bill->getBarcode());
@@ -125,8 +144,7 @@ class BillFactoryTest extends TestCase
     /** @test */
     public function its_creates_a_boleto_object_from_a_valid_barcode()
     {
-        $factory = new BillFactory();
-        $bill = $factory->make('00199000000000969610000003149039000458984217');
+        $bill = $this->factory->make('00199000000000969610000003149039000458984217');
 
         $this->assertInstanceOf(Boleto::class, $bill);
         $this->assertSame('00190000090314903900404589842170900000000096961', $bill->getWritableLine());
@@ -135,8 +153,7 @@ class BillFactoryTest extends TestCase
     /** @test */
     public function its_creates_a_convenio_object_from_a_valid_barcode()
     {
-        $factory = new BillFactory();
-        $bill = $factory->make('84660000001249800820899999319301093721978999');
+        $bill = $this->factory->make('84660000001249800820899999319301093721978999');
 
         $this->assertInstanceOf(Convenio::class, $bill);
         $this->assertSame('846600000018249800820899999931930104937219789990', $bill->getWritableLine());
@@ -150,7 +167,7 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage("The value `{$invalidLine}` given is not a valid barcode or writable line.");
 
-        (new BillFactory())->make($invalidLine);
+        $this->factory->make($invalidLine);
     }
 
     /** @test */
@@ -161,7 +178,7 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage("The value `{$invalidLine}` given is not a valid barcode or writable line.");
 
-        (new BillFactory())->make($invalidLine);
+        $this->factory->make($invalidLine);
     }
 
     /** @test */
@@ -172,7 +189,7 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage("The value `{$invalidBarcode}` given is not a valid barcode or writable line.");
 
-        (new BillFactory())->make($invalidBarcode);
+        $this->factory->make($invalidBarcode);
     }
 
     /** @test */
@@ -183,6 +200,6 @@ class BillFactoryTest extends TestCase
         $this->expectException(BoletoWinnerException::class);
         $this->expectExceptionMessage("The value `{$invalidBarcode}` given is not a valid barcode or writable line.");
 
-        (new BillFactory())->make($invalidBarcode);
+        $this->factory->make($invalidBarcode);
     }
 }
