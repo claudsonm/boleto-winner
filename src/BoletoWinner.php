7970068d05b2701207bc2f5a36a836bd50d1238b
Claudson Martins <claudson@outlook.com>
2020-11-02 14:51:19 -0300

    Escrito handler do isValidType na facade

--- src/BoletoWinner.php
+++ src/BoletoWinner.php
@@ -3,13 +3,14 @@
 namespace Claudsonm\BoletoWinner;
 
 use BadMethodCallException;
-use Claudsonm\BoletoWinner\Factories\BillFactory;
 use Claudsonm\BoletoWinner\Exceptions\BoletoWinnerException;
+use Claudsonm\BoletoWinner\Factories\BillFactory;
 
 /**
- * @method static array|string[] getBills()
- * @method static void load(string $class)
- * @method static Bill make(string $barcodeOrWritableLine)
+ * @method static bool isValidBoleto(string $barcodeOrWritableLine)
+ * @method static bool isValidConvenio(string $barcodeOrWritableLine)
+ * @method static bool isValidBarcode(string $barcode)
+ * @method static bool isValidWritableLine(string $barcode)
  *
  * @see BillFactory
  */
@@ -22,24 +23,44 @@ class BoletoWinner
      * @param array  $arguments
      *
      * @throws BadMethodCallException
+     * @throws BoletoWinnerException
+     *
+     * @return bool
      */
     public static function __callStatic($method, $arguments)
     {
+        if (starts_with('isValid', $method)) {
+            return self::handleIsValidTypeCall($method, $arguments);
+        }
+
         if (! method_exists(BillFactory::class, $method)) {
             throw new BadMethodCallException("Method `{$method}` does not exist.");
         }
 
-        return (new BillFactory())->{$method}(...$arguments);
+        return BillFactory::getInstance()->{$method}(...$arguments);
     }
 
-    public static function isValid(string $barcode): bool
+    public static function isValid(string $barcodeOrWritableLine): bool
     {
         try {
-            self::make($barcode);
+            BillFactory::getInstance()->createBillInstanceFromString($barcodeOrWritableLine);
 
             return true;
         } catch (BoletoWinnerException $exception) {
             return false;
         }
+    }
+
+    /**
+     * @throws BoletoWinnerException
+     */
+    private static function handleIsValidTypeCall(string $method, array $arguments): bool
+    {
+        $type = strtolower(substr($method, 7));
+        $input = preg_replace('/[^0-9]/', '', ...$arguments);
+        $billClass = BillFactory::getInstance()->createBillInstance($type);
+        $billClass->setBarcode($input)->setWritableLine($input);
+
+        return $billClass->isBarcodeValid() || $billClass->isWritableLineValid();
     }
 }
--- tests/BoletoWinnerTest.php
+++ tests/BoletoWinnerTest.php
@@ -8,20 +8,84 @@ use PHPUnit\Framework\TestCase;
 class BoletoWinnerTest extends TestCase
 {
     /** @test */
-    public function it_checks_when_a_bar_code_is_invalid()
+    public function it_checks_when_a_boleto_barcode_is_invalid()
     {
-        $barcode = '2213';
+        $barcode = '99991821300003000003381260023848044400006330';
 
-        // $this->assertTrue(BoletoWinner::isValid($barcode));
-        $this->assertTrue(true);
+        $this->assertFalse(BoletoWinner::isValid($barcode));
     }
 
     /** @test */
-    public function it_checks_when_a_writable_line_is_invalid()
+    public function it_checks_when_a_boleto_writable_line_is_invalid()
     {
-        $barcode = '2213';
+        $line = '34191.79001 01043.510047 99990.150008 1 84270026800';
 
-        // $this->assertTrue(BoletoWinner::isValid($barcode));
-        $this->assertTrue(true);
+        $this->assertFalse(BoletoWinner::isValid($line));
+    }
+
+    /** @test */
+    public function it_checks_when_a_boleto_barcode_is_valid()
+    {
+        $barcode = '23791821300003000003381260023848044400006330';
+
+        $this->assertTrue(BoletoWinner::isValid($barcode));
+    }
+
+    /** @test */
+    public function it_checks_when_a_boleto_writable_line_is_valid()
+    {
+        $line = '34191.79001 01043.510047 91020.150008 1 84270026000';
+
+        $this->assertTrue(BoletoWinner::isValid($line));
+    }
+
+    /** @test */
+    public function it_checks_when_a_convenio_barcode_is_invalid()
+    {
+        $barcode = '84630000001249800820899999319301091789119999';
+
+        $this->assertFalse(BoletoWinner::isValid($barcode));
+    }
+
+    /** @test */
+    public function it_checks_when_a_convenio_writable_line_is_invalid()
+    {
+        $line = '84630000001 1 24980082089 9 99993193010 4 91789116299 9';
+
+        $this->assertFalse(BoletoWinner::isValid($line));
+    }
+
+    /** @test */
+    public function it_checks_when_a_convenio_barcode_is_valid()
+    {
+        $barcode = '84630000001249800820899999319301091789116299';
+
+        $this->assertTrue(BoletoWinner::isValid($barcode));
+    }
+
+    /** @test */
+    public function it_checks_when_a_convenio_writable_line_is_valid()
+    {
+        $line = '84630000001 1 24980082089 9 99993193010 4 91789116299 7';
+
+        $this->assertTrue(BoletoWinner::isValid($line));
+    }
+
+    /** @test */
+    public function it_checks_if_the_given_barcode_represents_the_desired_bill_type()
+    {
+        $barcode = '23791821300003000003381260023848044400006330';
+
+        $this->assertTrue(BoletoWinner::isValidBoleto($barcode));
+        $this->assertFalse(BoletoWinner::isValidConvenio($barcode));
+    }
+
+    /** @test */
+    public function it_checks_if_the_given_writable_line_represents_the_desired_bill_type()
+    {
+        $line = '84690000001 5 24490082089 9 99993193010 4 99453529299 3';
+
+        $this->assertFalse(BoletoWinner::isValidBoleto($line));
+        $this->assertTrue(BoletoWinner::isValidConvenio($line));
     }
 }
