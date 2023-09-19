3b66f3b693fdfaa4d8495678c5564ba245b65d40
Lucas Ferro <lucasferrobrandao@hotmail.com>
2023-09-19 09:03:16 -0300

    feat(verification-boleto): agora verifica corretamente código de barras e linha digitável

--- src/Validators/ConvenioValidator.php
+++ src/Validators/ConvenioValidator.php
@@ -11,15 +11,33 @@ class ConvenioValidator implements Validator
      */
     public function verifyWritableLine(string $writableLine): bool
     {
+        if (strlen($writableLine) != 48) {
+            return false;
+        }
+
+        if ($writableLine[0] != '8') {
+            return false;
+        }
+
+        if (! in_array($writableLine[1], Convenio::SEGMENT_IDENTIFICATION)) {
+            return false;
+        }
+
         $blocks = str_split($writableLine, 12);
+
         $isModule10 = in_array($writableLine[2], Convenio::USES_MODULE_10);
 
         $blocksValidated = 0;
         foreach ($blocks as $block) {
+
             $blockLength = strlen($block) - 1;
+
             $blockInputNumbers = substr($block, 0, $blockLength);
+
             $blockVerificationDigit = $this->getLastCharacter($block);
+
             $digitCalculated = $isModule10 ? module10($blockInputNumbers) : module11($blockInputNumbers);
+
             if ($digitCalculated === $blockVerificationDigit) {
                 $blocksValidated++;
             }
@@ -33,14 +51,30 @@ class ConvenioValidator implements Validator
      */
     public function verifyBarcode(string $barcode): bool
     {
+        if (strlen($barcode) != 44) {
+            return false;
+        }
+
+        if ($barcode[0] != '8') {
+            return false;
+        }
+
+        if (! in_array($barcode[1], Convenio::SEGMENT_IDENTIFICATION)) {
+            return false;
+        }
+
         $effectiveValueOrReferenceIdentifier = $barcode[2];
+
         if (! in_array($effectiveValueOrReferenceIdentifier, Convenio::AVAILABLE_CODES)) {
             return false;
         }
 
         $barcodeVerificationDigit = $barcode[3];
+
         $blockInputNumbers = substr($barcode, 0, 3);
+
         $blockInputNumbers .= substr($barcode, 4);
+
         $digitCalculated = in_array($effectiveValueOrReferenceIdentifier, Convenio::USES_MODULE_10)
             ? module10($blockInputNumbers)
             : module11($blockInputNumbers);
